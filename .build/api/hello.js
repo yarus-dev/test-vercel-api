86eecc9ae0674c38e510f6b3ef94a6d0e61a184c
Yaroslav Usenko <yar.usenko.inc@gmail.com>
2024-03-03 18:08:31 +0200

    commit

--- .build/api/hello.js
+++ .build/api/hello.js
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: "nodejs", // this is a pre-requisite
+};
+
 export default function handler(req, res) {
   const { name = "World" } = req.query;
   return res.json({
